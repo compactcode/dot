99673a95e3ef1630a7f31a9d7d0783ddec740a35
Shanon McQuay <hi@shan.dog>
2021-12-16 00:25:25 +1100

    Tweak gitsigns config

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,12 +1,22 @@
 require('gitsigns').setup({
   keymaps = {},
+  current_line_blame = true,
+  current_line_blame_opts = {
+    delay = 500,
+  },
+  preview_config = {
+    border = 'none',
+  },
+  yadm = {
+    enable = true,
+  },
 })
 
 require("which-key").register({
   ["<leader>"] = {
     g = {
       name = "+git",
-      b = { "<cmd>Gitsigns blame_line<cr>", "Show blame for the current line" },
+      n = { "<cmd>Gitsigns next_hunk<cr>", "Show changes for the next hunk" },
       p = { "<cmd>Gitsigns preview_hunk<cr>", "Show changes for this hunk" },
       r = { "<cmd>Gitsigns reset_hunk<cr>", "Discard this hunk" },
     },

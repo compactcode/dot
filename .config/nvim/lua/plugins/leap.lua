5df579c724f2e54ef29415cecc9827efdd63a34b
Shanon McQuay <hi@shan.dog>
2022-11-14 08:49:49 +1100

    Update leap function to work with new version.

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,10 +1,12 @@
 -- Searching in all windows (including the current one) on the tab page:
 function leap_all_windows()
-  require('leap').leap {
-    ['target-windows'] = vim.tbl_filter(
-      function (win) return vim.api.nvim_win_get_config(win).focusable end,
-      vim.api.nvim_tabpage_list_wins(0)
-    )
+  local focusable_windows_on_tabpage = vim.tbl_filter(
+    function (win) return vim.api.nvim_win_get_config(win).focusable end,
+    vim.api.nvim_tabpage_list_wins(0)
+  )
+
+  require'leap'.leap {
+    target_windows = focusable_windows_on_tabpage
   }
 end
 
